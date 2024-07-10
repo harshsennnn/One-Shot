316de4d8c1c364244b91160b035e5bc0b6b1a85b
HARSH SEN <harshsennn@gmail.com>
2024-07-10 23:51:12 +0530

    all the files added

---
+++ back.js
@@ -0,0 +1,8 @@
+/*<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+ var typed = new Typed('#element', {
+    strings: ['One Shot: Your one-stop destination for all engineering resources consolidated in a single platform.'],
+    typeSpeed: 50,
+  });*/
+
+
+  
